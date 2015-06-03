81fbc5d99755c6674c1b0b1b268991b421c49a41
daniel <danielsoto03@gmail.com>
2015-06-02 20:11:29 -0700

    Removed implementation of ParallaxLayer in preparation of upgraded version. ParallaxLayer no longer needs x coordinate for initialization. added set_position method to ParallaxLayer.

--- background.py
+++ background.py
@@ -3,11 +3,10 @@ import pygame
 import sys
 from pygame.locals import *
 
-class ParallaxLayer():
+class ParallaxLayer(pygame.sprite.Sprite):
     
-    def __init__(self, x, y, image_location, movement_rate):
+    def __init__(self, y, image_location, movement_rate):
         pygame.sprite.Sprite.__init__(self)
-        self.x = x
         self.y = y
         self.movement_rate = movement_rate
         
@@ -19,34 +18,24 @@ class ParallaxLayer():
         self.rect.top = y
         self.rect.left = x
         
-    def updated(self):
-        self.rect.left -= self.movement_rate
-            
     def change_speed(self, x):
         self.movement_rate *= x
         
-class ParallaxLayers():
-    
-    def __init__(self, y, surface, tile_width, image_location, 
-                          movement_speed):
-        self.screen_width = surface.get_width()
-        self.y = y
-        self.tile_width = tile_width
-        self.image_location = image_location
-        self.movement_speed = movement_speed
+    def set_position(self, x):
+        self.rect.left = x
         
     def update(self):
-        ground_list = []
-        for i in range(len(self.y)):
-            x = 0
-            while x < self.screen_width:
-                layer = ParallaxLayer(x, self.y[i], self.image_location[i], 
-                                      self.movement_speed[i])
-                ground_list.append(layer)
-                x += self.tile_width[i]
-        return ground_list
+        self.rect.left += self.movement_rate
+            
+        
+class ParallaxLayers():
+    
+    def __init__(self):
+        
+        
+    def update(self):
+        
         
     def change_speed(self, modifier):
-        for i in len(self.movement_speed):
-            self.movement_speed *= modifier    
+       
    
