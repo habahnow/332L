e443c2a552443d5042213db34a4feb2c46871932
daniel <danielsoto03@gmail.com>
2015-06-03 13:38:36 -0700

    made the new changes to the background file. add_layer and initialization have been added

--- background.py
+++ background.py
@@ -16,11 +16,12 @@ class ParallaxLayer(pygame.sprite.Sprite):
         self.image = pygame.image.load(image_directory)
         self.rect = self.image.get_rect()
         self.rect.top = y
-        self.rect.left = x
+        self.rect.left = 0
+        self.width = self.rect.width
         
     def change_speed(self, x):
         self.movement_rate *= x
-        
+    
     def set_position(self, x):
         self.rect.left = x
         
@@ -30,12 +31,46 @@ class ParallaxLayer(pygame.sprite.Sprite):
         
 class ParallaxLayers():
     
-    def __init__(self):
+    __group_list =[]
+    __isPaused = False
+    __isSlow = False
+    __isFast = False
+    
+    def __init__(self, screen):
+        self.__screen = screen
+        self.__width = screen.get_width
         
+    def add_layer(self, layer, height, fill):
+        if fill:
+            extra_width = layer.width - 1
+            total_tile_coverage = extra_width + self.__width
+            group = pygame.sprite.Group
+            layer.set_position(0)
+            group.add(layer)
+            left_position = layer.width
+            while left_position <= total_tile_coverage:
+                new_layer = ParallaxLayer(layer.y, layer.image,
+                                          layer.movement_rate)
+                new_layer.set_position(left_position)
+                left_position += layer.width
+                group.add(new_layer)
+            group_list.append(group) 
         
-    def update(self):
+    def decrease_speed(self):
+        print ('decrease speed')
         
+    def get_groups(self):
+        return group_list[:]
         
-    def change_speed(self, modifier):
+    def increase_speed(self):
+        print ('increase speed')
+        
+    def pause(self):
+        print ('pause')
+        
+    def resume(self):
+        print ('resume')
        
-   
+    def update (self):
+        self.rect.left -= self.movement_rate
+        
